38787999a099fab4cb28b51fe448532f7f9d027f
KSI5 <krissigebs@gmail.com>
2023-07-21 05:31:40 -0700

    fixed 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,62 +1,52 @@
-#include <stdio.h>
 #include "sort.h"
 
 /**
- * print_list - Prints a doubly linked list of integers.
- * @list: A pointer to the head of the doubly linked list.
+ * swap_nodes - Swap two nodes in a listint_t doubly-linked list.
+ * @h: A pointer to the head of the doubly-linked list.
+ * @n1: A pointer to the first node to swap.
+ * @n2: The second node to swap.
  */
-void print_list(const listint_t *list)
+void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	while (list)
-	{
-		printf("%d", list->n);
-		if (list->next)
-			printf(", ");
-		list = list->next;
-	}
-	printf("\n");
+	(*n1)->next = n2->next;
+	if (n2->next != NULL)
+		n2->next->prev = *n1;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
+
+	if ((*n1)->prev != NULL)
+		(*n1)->prev->next = n2;
+
+	else
+		*h = n2;
+
+	(*n1)->prev = n2;
+	*n1 = n2->prev;
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in ascending
- * order using an insertion sort algorithm.
- * @list: A pointer to the doubly linked list to be sorted.
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *lead, *follow, *new, *temp;
+	listint_t *iter, *insert, *tmp;
 
-	if (!list || !(*list) || !((*list)->next))
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	follow = *list;
-	lead = (*list)->next;
-
-	while (lead)
+	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
-		new = lead->next;
-		while (follow && lead->n < follow->n)
+		tmp = iter->next;
+		insert = iter->prev;
+
+		while (insert != NULL && iter->n < insert->n)
 		{
-			if (follow->prev)
-				follow->prev->next = lead;
-			else
-				*list = lead;
-
-			if (lead->next)
-				lead->next->prev = follow;
-
-			temp = lead->next;
-			lead->next = follow;
-			lead->prev = follow->prev;
-			follow->next = temp;
-			follow->prev = lead;
-
-			print_list(*list);
-
-			follow = lead->prev;
+			swap_nodes(list, &insert, iter);
+			print_list((const listint_t *)*list);
 		}
-		lead = new;
-		if (lead)
-			follow = lead->prev;
 	}
 }
